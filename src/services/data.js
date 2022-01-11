f22f63cb518930b4ff8675eb99d7ec93bb189bd0
Violet Stewart <vkazoo2@gmail.com>
2022-01-11 14:10:47 -0800

    start

---
+++ src/services/data.js
@@ -0,0 +1,10 @@
+import { client, checkError } from './client';
+
+export async function getAlbums() {
+  let request = await client.from('albums').select('*');
+  return checkError(request);
+}
+
+export async function getAlbumsById(id) {}
+let request = await client.from('dog-data').select('*').match({ id }).single();
+return checkError(request);
